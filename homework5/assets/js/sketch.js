902fde1e043dafe54b4341cd7162b7df27c9fe8b
Mike Rhiney <rhineyman@gmail.com>
2026-02-17 20:06:14 -0700

    last bits
    nightmare

--- homework5/assets/js/sketch.js
+++ homework5/assets/js/sketch.js
@@ -3,6 +3,14 @@ var idle1;
 var count = 0;
 var idleImages = [];
 var fruitImages = [];
+
+var fruitPositions = [
+    {x: 500, y: 200},
+    {x: 400, y: 0},
+    {x: 200, y: 500},
+    {x: 50, y: 200}
+];
+
 var frameCounter = 0;
 
 var x = 100;
@@ -10,11 +18,13 @@ var y = 100;
 
 function preload() {
     
-    
+   //idle 
     for (var i = 0; i < 10; i++) {
         idleImages[i] = loadImage("assets/images/Idle__" + String(i).padStart(3, '0') + ".png");
 
     }
+
+//fruit
 
     for (var i = 0; i < 4; i++) {
         fruitImages[i] = loadImage("assets/images/Fruit" + String(i) + ".png");
@@ -34,9 +44,19 @@ function draw() {
     } 
     for (var i = 0; i < fruitImages.length; i++) {
         fruitImages[i].resize(0, 100);
+
     }
+
+    //generate idle and fruit images
     image(idleImages[count], 400, 300);
     image(fruitImages[count % fruitImages.length], x, y);
+
+    for (var i = 0; i < fruitImages.length; i++) {
+        var pos = fruitPositions[i] || {x: 0, y: 0};
+        image(fruitImages[i], pos.x, pos.y);
+    }
+
+//timer
 
     frameCounter++;
     if (frameCounter >= 5) {
@@ -46,6 +66,8 @@ function draw() {
             count = 0;
         }
     }
+
+    //move fruit 
 
     if (x < 400) {
         x += 2;
