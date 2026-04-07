85caff2f044c96e9cd108cf565dfe9026b775e52
Mike Rhiney <rhineyman@gmail.com>
2026-04-06 20:28:41 -0600

    everything but text
    dont understand what is wrong with text

--- homework12/js/sketch.js
+++ homework12/js/sketch.js
@@ -1,5 +1,9 @@
+let text;
+
+
 function setup() {
   createCanvas(400, 400, WEBGL);
+
 }
 
 function draw() {
@@ -7,20 +11,65 @@ function draw() {
 
   function drawCone() {
     push();
-    orbitControl();
+    //orbitControl();
     translate(-100, 100);
-    //rotateX(frameCount * 0.01);
-    //rotateY(frameCount * 0.02);
-    //rotateZ(frameCount * 0.03);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.02);
+    rotateZ(frameCount * 0.03);
     cone(40, 70);
     pop();
 }
-function drawCyclinder() {
-    //normalMaterial();
+function drawCylinder() {
+    push();
+    
+    orbitControl();
+    normalMaterial();
     translate(100, 100);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.02);
+    rotateZ(frameCount * 0.03);
     cylinder(20, 50, 24, 16, false, false);
+    
+    pop();
 }
+function drawCube() {
+    push();
+    ambientLight(255, 0, 255);
+    ambientMaterial(100, 150, 200);
+    translate(0, 100);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    rotateZ(frameCount * 0.01);
+    box(50, 50, 50);
+    pop();
+}
+function drawTorus() {
+    push();
+    //orbitControl();
+    specularMaterial(0,0,255);
+    translate(0, -100);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    torus(30, 10);
+    pop();
+}
+
+function drawEllipsoid() {
+    push();
+    //orbitControl();
+    ambientLight(255, 0, 255);
+    ambientMaterial(100, 150, 200);
+    translate(-100, -100);
+    rotateX(frameCount * 0.01);
+    ellipsoid(30, 20, 10);
+    pop();
+}
+
 drawCone();
-drawCyclinder();
+drawCylinder();
+drawCube();
+drawTorus();
+drawEllipsoid();
+
 
 }
